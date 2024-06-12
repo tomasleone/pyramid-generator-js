2fe6505217b7773ff0228aabba67d9795156b404
Tomas Leone <tomasnahuelleone@gmail.com>
2024-06-12 20:22:16 -0300

    creando piramide

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-let character = 'Hello';
-let count = 8;
-let rows = ["Naomi", "Quincy", "CamperChan"];
-let pushed = rows.push("freeCodeCamp");
-console.log(pushed);
-let popped = rows.pop();
-console.log(popped);
-console.log(rows);
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow() {
+
+}
+
+const call = padRow();
+console.log(call);
+
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1))
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
